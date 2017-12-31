eda66b67c98ec430a6d713c361af5aa7cb0e0f0b
Krzysztof Nowak <info@knowak.net>
2017-12-31 15:29:34 +0100

    Added many questions and next button. Almost chapter 02 has been completed - icons for buttons are missed right now.

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2017. Krzysztof Nowak "knowaknet"
+ */
+
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by knowaknet on 30.12.2017.
+ */
+
+public class Question {
+    private int mTextResId;
+    private boolean mIsAnswerTrue;
+
+    public Question(int textResId, boolean isAnswerTrue) {
+        this.mTextResId = textResId;
+        this.mIsAnswerTrue = isAnswerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mIsAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mIsAnswerTrue = answerTrue;
+    }
+}
--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017. Krzysztof Nowak "knowaknet"
+ */
+
 package com.bignerdranch.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
@@ -5,12 +9,17 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
-    private Button btnTrue;
-    private Button btnFalse;
+    private Button mBtnTrue;
+    private Button mBtnFalse;
+    private Button mBtnNext;
+    private TextView mQuestionTextView;
+    private Question[] mQuestions;
+    private int mCurrentIndexQuestion = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,32 +28,71 @@ public class QuizActivity extends AppCompatActivity {
 
         setupWidgets();
         setupWidgetsEvents();
+        loadQuestions();
     }
 
     private void setupWidgets(){
-        btnTrue = findViewById(R.id.btn_true);
-        btnFalse = findViewById(R.id.btn_false);
+        mBtnTrue = findViewById(R.id.btn_true);
+        mBtnFalse = findViewById(R.id.btn_false);
+        mBtnNext = findViewById(R.id.btn_next);
+        mQuestionTextView = findViewById(R.id.question_text_view);
     }
 
     private void setupWidgetsEvents(){
-        btnTrue.setOnClickListener(new View.OnClickListener() {
+        mBtnTrue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showAnswerResult(R.string.msg_correct_answer);
+                checkAnswer(true);
             }
         });
 
-        btnFalse.setOnClickListener(new View.OnClickListener() {
+        mBtnFalse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showAnswerResult(R.string.msg_incorrect_answer);
+                checkAnswer(false);
             }
         });
+
+        mBtnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndexQuestion = (mCurrentIndexQuestion + 1) %  mQuestions.length;
+                reloadActiveQuestion();
+            }
+        });
+    }
+
+    private void checkAnswer(boolean userPressedTrue){
+        boolean validAnswer = mQuestions[mCurrentIndexQuestion].isAnswerTrue();
+        if(userPressedTrue == validAnswer){
+            showAnswerResult(R.string.msg_correct_answer);
+        } else {
+            showAnswerResult(R.string.msg_incorrect_answer);
+        }
     }
 
     private void showAnswerResult(int resId){
         Toast msg = Toast.makeText(QuizActivity.this, resId, Toast.LENGTH_SHORT);
         msg.setGravity(Gravity.TOP, 0, 100);
         msg.show();
+    }
+
+    private void loadQuestions(){
+        mQuestions = new Question[]{
+                new Question(R.string.australia, true),
+                new Question(R.string.oceans, true),
+                new Question(R.string.mideast, false),
+                new Question(R.string.africa, false),
+                new Question(R.string.americas, true),
+                new Question(R.string.asia, true)
+        };
+
+        mCurrentIndexQuestion = 0;
+        reloadActiveQuestion();
+    }
+
+    private void reloadActiveQuestion(){
+        int activeQuestionRId = mQuestions[mCurrentIndexQuestion].getTextResId();
+        mQuestionTextView.setText(activeQuestionRId);
     }
 }
