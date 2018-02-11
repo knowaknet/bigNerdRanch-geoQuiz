4950c88708a637b4962e83548df9919a662abca4
Krzysztof Nowak <info@knowak.net>
2018-02-11 15:52:07 +0100

    fill cheat activity hints

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -8,10 +8,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CheatActivity extends AppCompatActivity {
 
     private static final String EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true";
+
+    private boolean mAnswerIsTrue;
+    private Button mShowAnswer;
+    private TextView mAnswer;
 
     public static Intent newIntent(Context packageContext, boolean answerIsTrue){
         Intent cheatIntent = new Intent(packageContext, CheatActivity.class);
@@ -23,5 +30,23 @@ public class CheatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+
+        setupWidgets();
+        setupWidgetEvents();
+    }
+
+    private void setupWidgets() {
+        mShowAnswer = findViewById(R.id.btn_show_answer);
+        mAnswer = findViewById(R.id.answer_view_text);
+    }
+
+    private void setupWidgetEvents(){
+        mShowAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAnswer.setText( mAnswerIsTrue ? R.string.btn_label_true : R.string.btn_label_false );
+            }
+        });
     }
 }
